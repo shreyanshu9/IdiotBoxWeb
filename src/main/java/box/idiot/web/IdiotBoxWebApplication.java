12c0297c939cc1b433d58857959515156637c4a2
Shreyanshu <ayush@ubuntu.lan.coriolis.co.in>
2019-03-14 12:07:07 +0530

    Idiot Box Web Project Initialization

---
+++ src/main/java/box/idiot/web/IdiotBoxWebApplication.java
@@ -0,0 +1,13 @@
+package box.idiot.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdiotBoxWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdiotBoxWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/box/idiot/web/IdiotBoxWebApplicationTests.java
@@ -0,0 +1,16 @@
+package box.idiot.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IdiotBoxWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
